05b9d97e98d987158313055c0a56207392fb4bf2
MFerguson1449 <mferguson1449@gmail.com>
2020-08-02 15:12:41 -0400

    course progress, more function work

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,1 +1,26 @@
-console.log("You're doing it!");
+// Function Definition
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
+  console.log('Running Calculate Bill!!');
+  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
+  return total;
+}
+
+// Function Call. Or **Run**
+// const myTotal = calculateBill(100, 0.13);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo('Wes');
+// console.log(greeting);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
