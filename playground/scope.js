48b09af3bfa3c28329f73d294e7c7425eac998a2
MFerguson1449 <mferguson1449@gmail.com>
2020-08-09 12:14:58 -0400

    scoping, hoisting, closures

--- playground/scope.js
+++ playground/scope.js
@@ -1,1 +1,32 @@
-const first = 'wes';
+// const age = 100;
+
+// function go() {
+//   const hair = 'blonde';
+// }
+
+// go();
+
+// console.log(age);
+// console.log(hair);
+
+// function isCool(name) {
+//   let cool;
+//   if (name === 'wes') {
+//     cool = true;
+//   }
+//   console.log(cool);
+//   return cool;
+// }
+
+const dog = 'Snickers';
+
+function logDog(dog) {
+  console.log(dog);
+}
+
+function go() {
+  const dog = 'Sunny';
+  logDog('Rufus');
+}
+
+go();
